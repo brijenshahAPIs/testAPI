d8e562edec9462f3462603e14adafbab84c36fef
Brijen Shah <135441289+brijenshahAPIs@users.noreply.github.com>
2023-07-30 13:28:44 -0400

    Update  Email.js

--- src/models/Email.js
+++ src/models/Email.js
@@ -3,9 +3,10 @@ const Schema = mongoose.Schema;
 
 
 const emailSchema = new Schema({
+     reciever : {type: String, required : true},
     subject : {type : String, required : true},
     body : {type : String, required : true},
     user :{ type : Schema.Types.ObjectId, ref :'User',required : true}
-});
+}{timestamps : true});
 
 module.exports = mongoose.model('Email',emailSchema);
