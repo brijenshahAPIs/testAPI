481fb463453a803fd03e839dacf5c809c2fcd8d3
Brijen Shah <135441289+brijenshahAPIs@users.noreply.github.com>
2023-07-30 22:12:42 -0400

    Update Email.js

--- src/models/Email.js
+++ src/models/Email.js
@@ -3,10 +3,37 @@ const Schema = mongoose.Schema;
 
 
 const emailSchema = new Schema({
-     reciever : {type: String, required : true},
-    subject : {type : String, required : true},
-    body : {type : String, required : true},
-    user :{ type : Schema.Types.ObjectId, ref :'User',required : true}
-},{timestamps : true});
+    sender: {
+        type: String,
+        required : true
+    },
+    reciever : 
+    {type: String, 
+        required : true
+    },
+    subject : {
+        type : String, 
+        required : true
+    },
+    body : {
+        type : String, 
+        required : true
+    },
+    isDeletedBySender : {
+        type : Boolean,
+        required : true
+    },
+    isDeletedByReceiever : {
+        type : Boolean,
+        required : true
+    },
+    user :{ 
+        type : Schema.Types.ObjectId,
+         ref :'User',
+         required : true}
+}, 
+{
+    timestamps : true
+});
 
 module.exports = mongoose.model('Email',emailSchema);
